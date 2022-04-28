870d226d1677e85164e70df979fe5b0d7a64bec2
Amery Gration <gration@tplxdt055.nat.physics.ox.ac.uk>
2022-04-28 18:38:14 +0100

    First PyPI release version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,8 @@ import sphinx_rtd_theme
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
+extensions = ['sphinx_toolbox.more_autodoc',
+              'sphinx.ext.autodoc',
               'sphinx.ext.mathjax',
               'sphinx.ext.viewcode',
               'sphinx.ext.napoleon',
